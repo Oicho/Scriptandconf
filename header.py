ca13b723a9fb965353808a6272b439fd2d465d82
Gauthier Fridiere <gauthier.fridiere@gmail.com>
2013-03-18 05:55:50 +0100

    Better structure

--- header.py
+++ header.py
@@ -3,19 +3,32 @@
 import argparse
 import datetime
 
-def print_init_header(arguments):
-    now = datetime.datetime.now()
-    file = open(args.file_name + ".hh", "w")
-    file.write("/*\n**    " + args.file_name + ".hh" + "\n**      Created on: ")
-    file.write()
-    file.write("**      By Gauthier \"Oicho\" FRIDIERE\n**      Contact at:")
-    
+#We create a function to clearify the code
+#Parsing arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("file_name", type=str, 
                     help="the class name")
 parser.add_argument("-p","--project_name", type=str,
                     help="the project name", default="None")
 args = parser.parse_args()
-print (args.file_name)
-print (args.project_name)
-print_init_header(args)
+now = datetime.datetime.now()
+file = open(args.file_name + ".hh", "w")
+try:
+    file.write("/*\n**    " + args.file_name + ".hh" + "\n**      Created on: ")
+    file.write(str(now.day) + "/" + str(now.month) + "/" + str(now.year))
+    file.write("\n**      By Gauthier \"Oicho\" FRIDIERE\n**      Contact at: ")
+    file.write("gauthier.fridiere@gmail.com\n")
+    file.write("**    Project name: " + args.project_name + "\n*/\n\n")
+    # Now we write preprocessor
+    classname = args.file_name.capitalize()
+    up = args.file_name.upper() + "_HH"
+    file.write("#ifndef " + up +"\n# define " + up + "\n\nclass " + classname)
+    file.write("\n{\n  public://#! const/dec")
+    file.write("\n    " + classname + "();\n    virtual ~" + classname)
+    # The comment is for the generatecc script to generate getter setter
+    file.write("();\n\n//#! members declarations\n  private:\n\n")
+    file.write("//#! getter/setter\n  public:\n\n//#! methods\n  public:\n\n};\n\n")
+    file.write("#endif //! " + up)
+except OSError:
+    print ("Can't create the header file.")
+    quit()
