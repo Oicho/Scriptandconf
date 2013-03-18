fa063e9e91151e680c0fbe3dda0749e7e8a830f9
Gauthier Fridiere <gauthier.fridiere@gmail.com>
2013-03-18 07:06:53 +0100

    Basic writing functions done, only parser remaining

---
+++ generate_source.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python3
+
+global file_hxx
+
+def getterwrite(classname, identifier, idtype):
+    file_hxx.write("inline\nvoid\n" + classname + "::" + identifier + "get(")
+    parameter = " param_" + identifier
+    file_hxx.write(idtype + parameter + ")\n{\n  this->" + identifier + " = ")
+    file_hxx.write(parameter + ";\n}\n\n")
+
+def setterwrite(classname, identifier, idtype):
+    file_hxx.write("inline\nvoid\n" + classname + "::" + identifier + "set()\n{\n")
+    file_hxx.write("  return this->" + identifier + ";\n}\n\n")
+
+def consdeswrite(classname, identifier):
+    file_hxx.write(classname + "::" + identifier + "\n{\n\n\n}\n")
+
+def methodswrite(classname, typeid, identifier):
+    file_hxx.write(typeid + classname + "::" + identifier)
+
