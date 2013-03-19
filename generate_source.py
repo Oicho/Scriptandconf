9688275bf93abd4e36e674850f5aa07c542b6ef6
Gauthier Fridiere <gauthier.fridiere@gmail.com>
2013-03-19 02:10:11 +0100

    Performance improvement

--- generate_source.py
+++ generate_source.py
@@ -1,6 +1,5 @@
 #! /usr/bin/python3
 
-global file_hxx
 
 def getterwrite(classname, identifier, idtype):
     file_hxx.write("inline\nvoid\n" + classname + "::" + identifier + "get(")
@@ -8,13 +7,15 @@ def getterwrite(classname, identifier, idtype):
     file_hxx.write(idtype + parameter + ")\n{\n  this->" + identifier + " = ")
     file_hxx.write(parameter + ";\n}\n\n")
 
+
 def setterwrite(classname, identifier, idtype):
     file_hxx.write("inline\nvoid\n" + classname + "::" + identifier + "set()\n{\n")
     file_hxx.write("  return this->" + identifier + ";\n}\n\n")
 
+
 def consdeswrite(classname, identifier):
     file_hxx.write(classname + "::" + identifier + "\n{\n\n\n}\n")
 
+
 def methodswrite(classname, typeid, identifier):
     file_hxx.write(typeid + classname + "::" + identifier)
-
--- header.py
+++ header.py
@@ -8,27 +8,32 @@ import datetime
 parser = argparse.ArgumentParser()
 parser.add_argument("file_name", type=str,
                     help="the class name")
-parser.add_argument("-p","--project_name", type=str,
+parser.add_argument("-p", "--project_name", type=str,
                     help="the project name", default="None")
 args = parser.parse_args()
 now = datetime.datetime.now()
 file = open(args.file_name + ".hh", "w")
+
 try:
-    file.write("/*\n**    " + args.file_name + ".hh" + "\n**      Created on: ")
+    file.write("/*\n**    " + args.file_name + ".hh\n**      Created on: ")
     file.write(str(now.day) + "/" + str(now.month) + "/" + str(now.year))
-    file.write("\n**      By Gauthier \"Oicho\" FRIDIERE\n**      Contact at: ")
-    file.write("gauthier.fridiere@gmail.com\n")
-    file.write("**    Project name: " + args.project_name + "\n*/\n\n")
+    file.write("""\n**      By Gauthier \"Oicho\" FRIDIERE
+**      Contact at: gauthier.fridiere@gmail.com
+**    Project name: """)
+
+    file.write(args.project_name + "\n*/\n\n")
     # Now we write preprocessor
     classname = args.file_name.capitalize()
     up = args.file_name.upper() + "_HH"
-    file.write("#ifndef " + up +"\n# define " + up + "\n\nclass " + classname)
-    file.write("\n{\n// \const /dec\n  public:")
-    file.write("\n    " + classname + "();\n    virtual ~" + classname)
+    file.write("#ifndef " + up + "\n# define " + up + "\n\nclass " + classname)
+    file.write("\n{\n// \const /dec\n  public:\n    ")
+    file.write(classname + "();\n    virtual ~" + classname)
     # The comment is for the generatecc script to generate getter setter
     file.write("""();\n\n// \members declarations\n  private:\n
-// \getter /setter\n  public:\n\n//#! methods\n  public:\n\n};\n\n #endif //! """)
+// \getter /setter\n  public:\n\n//#! methods\n  public:\n\n};\n
+#endif //! """)
     file.write(up)
+
 except OSError:
     print ("Can't create the header file.")
     quit()
