c142f6e191d18f44300d6e274652249cfb420b1e
Fridiere Gauthier <fridie_g@epita.fr>
2013-03-25 17:38:57 +0100

    Member class done

--- generate_source.py
+++ generate_source.py
@@ -11,32 +11,42 @@ class Member(object):
 
     attributes: name_, typeid_
     """
-    def __init__(self, name, typeid="void"):
-        self.name_ = name
-        self.typeid_ = typeid
+    def __init__(self, input):
+        temp = input.split()
+        self.name_ = temp[len(temp) - 1]
+        self.typeid_ = temp
 
     def generate_set_proto(self):
-        temp = "void " + self.name_ + "set (" + self.typeid_ + "input);"
+        temp = self.name_ + "set(" + self.typeid_ + "input);\n"
         return temp
 
     def generate_get_proto(self):
-        temp = self.typeid_ + " " + self.name_ + "get ();"
+        temp = self.typeid_ + " " + self.name_ + "get();\n"
         return temp
+
     def gen_get_def(self, classname):
+        temp = "inline\n" + self.typeid_ + "\n" + classname + "::" + self.name_
+        temp += "get()\n{\n    return this->" + self.name_ + ";\n}\n\n"
+        return temp
 
     def gen_set_def(self, classname):
+        temp = "inline\nvoid\n" + classname + "::" + self.name_ + "set(" + self.typeid_
+        temp += "input)\n{\n    this->" + self.name_ + " = input;\n}\n\n"
+        return temp
 
-def parse_header(header_file):
+
+def parse_header(header_file, file_cc, file_hxx, classname):
     l_string = header_file.readlines()
     i = 0
     methods_list = []
     members_list = []
-#    constructor = ""
-#    destructor = ""
     for i in range(len(l_string)):
         if l_string[i].find("}"):
             break
         aux_parse(l_string, i, methods_list, members_list)
+    for i in range(len(members_list)):
+        file_hxx += members_list[i].gen_set_def(classname)
+        file_hxx += members_list[i].gen_get_def(classname)
 
 
 def aux_parse(l_string, i, methods_list, members_list):
@@ -57,7 +67,7 @@ def aux_parse(l_string, i, methods_list, members_list):
         i += 2
         while i < len(l_string) and not l_string[i].find("// \\") and not l_string[i].find("}"):
             if l_string[i] != "\n" and not l_string[i].find("//"):
-                members_list.append(l_string[i])
+                members_list.append(Member(l_string[i]))
             i += 1
 
 
@@ -89,7 +99,8 @@ parser.add_argument("-p", "--project_name", type=str,
                     help="the project name", default="None")
 parser.add_argument("-f", "--force",
                     help="You can use this option if you want the script\
-                    to overwrite your header file without asking")
+                    to overwrite your header file without asking",
+                    action="store_true")
 parser.add_argument("-v", "--verbose", type=int, help="Level of information you \
                     want to be print on the standard output between 0 and 3",
                     default=0)
